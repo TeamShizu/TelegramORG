3ae3489ffdce65973247e19c6c1b051d27e3e777
TeamShizu <87710820+TeamShizu@users.noreply.github.com>
2021-08-08 08:22:53 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,8 +1,10 @@
 class Translation(object):
     START_TEXT = """Hi!
-This Is An APP ID & HASH GENERATOR BOT.I Can Help You Get Your Telegram APP ID & HASH,
+This Is An APP ID & HASH GENERATOR BOT. I Can Help You Get Your Telegram APP ID & HASH,
+
 Enter your phone number with country code(example:-+94761234643)
 to Get The APP-ID From my.telegram.org 
+
 \n\n Press /start At Any Stage To Re-Enter Your Details If You Perhaps Made Some Mistake. Made by @ShizuUpdates"""
     AFTER_RECVD_CODE_TEXT = """A Verification Code sent to you by Telegram. Enter that Verification Code Or Forward That Verification Message.😁🙏
 """
